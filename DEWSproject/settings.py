8805e187b7773e68021e86cf9e4f7ce119cb5b51
Godswill Nasara <godswillnlm@gmail.com>
2022-02-06 22:22:56 +0100

    static and media url settings

--- DEWSproject/settings.py
+++ DEWSproject/settings.py
@@ -134,7 +134,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
