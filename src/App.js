53bf9888a47f16c15c99f54e57e6de2bb0a20947
Godswill Nasara <godswillnlm@gmail.com>
2022-02-10 12:25:16 +0100

    changed state and year values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [info, setInfo] = useState('');
+
+  function fetchData(payload) {
+    // let url = 'http://127.0.0.1:8000/dews/pred/';
+    let url = 'https://dewsapp.herokuapp.com/dews/pred/';
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload)
+    }).then(res => {
+      return res.json()
+    }).then(data => {
+      return setInfo(data)
+    }).catch(err => console.error('Error found:',err))
+  }
+
+  const payload = {
+    'state': 'KEBBI',
+    'year': 2025
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Drought App Here!!!</h2>
+
+      <button onClick={() => { fetchData(payload) }} >Submit</button>
+      {info ? <div>
+            <h2>{ info.drought_index }</h2>
+            <p>{ info.ocean_temperature }</p>
+            <p>{ info.climate_direction }</p>
+            <p>For the year: { info.year }</p>
+          </div>
+         : <p>No data yet...</p> }
     </div>
   );
 }
