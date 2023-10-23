c7548ef5bf0935e252c92646223b1acd8c11a22b
Godswill Nasara <godswillnlm@gmail.com>
2023-10-23 17:32:41 +0100

    Update: remove old endpoints and add new one

--- DEWSapp/urls.py
+++ DEWSapp/urls.py
@@ -5,7 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('details/', views.listView, name='list-view'),
-    path('pred/', csrf_exempt(views.predictView), name='predict-view'),
-    path('spei/', csrf_exempt(views.speiView), name='spei-view'),
+    # path('details/', views.listView, name='list-view'),
+    # path('pred/', csrf_exempt(views.predictView), name='predict-view'),
+    # path('spei/', csrf_exempt(views.speiView), name='spei-view'),
+
+    # path("predict", views.PredictAPIView.as_view(), name="predict"),
+    path("predict", views.PredictGenericAPIView.as_view(), name="predict-drought"),
 ]
