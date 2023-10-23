a2ba3a87d93fba964b385e193a7f9b53688d4b17
Godswill Nasara <godswillnlm@gmail.com>
2023-10-23 17:24:47 +0100

    Feature: create utility file to handle response type in openapi docs

---
+++ DEWSapp/utils/openapi_response.py
@@ -0,0 +1,16 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
+
+def response_schema(**kwargs):
+    def decorator(view):
+        extend_schema_view(
+            get=extend_schema(responses={200: kwargs["serializer"]}),
+            post=extend_schema(responses={201: kwargs["serializer"]}),
+            # list=extend_schema(responses={200: kwargs["serializer"]}),
+            # retrieve=extend_schema(responses={200: kwargs["serializer"]}),
+            # create=extend_schema(responses={201: kwargs["serializer"]}),
+            # update=extend_schema(responses={200: kwargs["serializer"]}),
+            # partial_update=extend_schema(responses={200: kwargs["serializer"]}),
+        )(view)
+
+        return view
+    return decorator
