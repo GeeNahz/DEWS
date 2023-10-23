0068fdb2dc8894d6bc77dde7f4cea7f7cf3a6b88
Godswill Nasara <godswillnlm@gmail.com>
2023-10-23 17:36:14 +0100

    Feature: create genericAPIView CBV for prediction

--- DEWSapp/views.py
+++ DEWSapp/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 import json
+from django.http import HttpResponse
 
 from django.views.decorators.csrf import ensure_csrf_cookie
-from rest_framework import status
-from rest_framework.decorators import api_view
+
+from rest_framework import status, permissions
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializer import TextSerializer, PredictSerializer, SpeiSerializer
-from .dataset import TextDetails, PredictDetails, SpeiDetails
+from rest_framework.decorators import api_view
+
+from .utils.openapi_response import response_schema
+
+from .serializer import (
+    PredictSerializer,
+    PredictSerializerRequest,
+    SpeiSerializer,
+    TextSerializer,
+)
+from .dataset import (
+    TextDetails,
+    PredictDetails,
+    SpeiDetails,
+)
 # import json
 
 from .predictions import predict, spei_predict
@@ -19,73 +33,60 @@ def homepage(request):
     return HttpResponse('<h2>Welcome!</h2>')
 
 
-@api_view(['GET'])
-def listView(request):
-    # details = TextDetails('Godswill', 20)
-    # serializer = TextSerializer(details)
-
-    if request.method == 'GET':
-
-        month = 'jan'
-        year = 2029
+@response_schema(serializer=PredictSerializer)
+class PredictGenericAPIView(GenericAPIView):
+    serializer_class = PredictSerializerRequest
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self):
+        """
+        Test endpoint with default hard-coded values. It only provides the response based on those default values.
+        """
+        
+        year = 2025
         state = 'borno'.upper()
 
-        drought_index, oceanTemp, climate_direction = predict(state, month, year)
+        with open("statesAndLgas.json") as j:
+            data = json.load(j)
 
-        # predict(state, month, year)
+        for states in data:
+            if states["state"].upper() == state:
+                current_state = states
 
-        results = PredictDetails(year, drought_index, oceanTemp, climate_direction)
-        serializer = PredictSerializer(results)
-
-        return Response(serializer.data)
-        # return HttpResponse("Home page?")
-
-
-@ensure_csrf_cookie
-@api_view(['GET', 'POST'])
-def predictView(request):
-    if request.method == 'POST':
-        # dictionary of months
-        # months = {
-        #     "january": "jan",
-        #     "february": "feb",
-        #     "march": "march",
-        #     "april": "april",
-        #     "may": "may",
-        #     "june": "june",
-        #     "july": "july",
-        #     "august": "aug",
-        #     "september": "sept",
-        #     "october": "oct",
-        #     "november": "nov",
-        #     "december": "dec",
-        # }
-
-        # data from request
-        data = json.loads(request.body)
-
-        # individual data
-        # month_from_request = data['month'].lower()
-        state = data['state'].upper()
-        year = int(data['year'])
-        lga = data['lga'].upper()
-
-        # month = months[month_from_request]
-
-        # calculations and prediction
+        lgas = current_state["lgas"]
+        lga = lgas[5].upper()
+        
         drought_index, oceanTemp, climate_direction = predict(state, year)
         SPEIA, SPEIM, SPEIJN, SPEIJY, SPEIAG, SPEIS, SPEIO, region = spei_predict(lga, year)
 
-        # prepare the return values to be serialized
         results = PredictDetails(year, drought_index, oceanTemp, climate_direction, SPEIA, SPEIM, SPEIJN, SPEIJY, SPEIAG, SPEIS, SPEIO, region)
 
-        # serializer the results
+        serializer = self.serializer_class(results, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        """
+        The drought prediction endpoint to predict drought using ITCZ5, ITCZ10 and SPEI.
+
+        Requires the state, year and lga for the prediction.
+        """
+        
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        year = serializer.data["year"]
+        lga = serializer.data["lga"].upper()
+        state = serializer.data["state"].upper()
+        
+        drought_index, oceanTemp, climate_direction = predict(state, year)
+        SPEIA, SPEIM, SPEIJN, SPEIJY, SPEIAG, SPEIS, SPEIO, region = spei_predict(lga, year)
+
+        results = PredictDetails(year, drought_index, oceanTemp, climate_direction, SPEIA, SPEIM, SPEIJN, SPEIJY, SPEIAG, SPEIS, SPEIO, region)
+
         serializer = PredictSerializer(results)
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    return HttpResponse("<h1>Want to Predict? Here's how:</h1><br/><p>Make a POST request making the body of the request to have the object format as follows:</p><br/><p>{<br/>&nbsp;&nbsp;&nbsp;'year':&nbsp;year_value,<br/>&nbsp;&nbsp;&nbsp;'state':&nbsp;state_value<br/>&nbsp;&nbsp;&nbsp;'lga':&nbsp;lga_value<br/>}</p>")
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @ensure_csrf_cookie
