8c8349856f0d4afcbbdda370414c8ed07b43d2e8
Othniel Eggah <emmaueleggah@gmail.com>
2023-12-20 19:40:13 +0100

    Feat: register soil moisture model to admin panel

--- apps/dewsDC/admin.py
+++ apps/dewsDC/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Temperature, Precipitation
+from .models import (
+    Temperature,
+    Precipitation,
+    SoilMoisture,
+)
 
 
 admin.site.register(Temperature)
 admin.site.register(Precipitation)
+admin.site.register(SoilMoisture)
