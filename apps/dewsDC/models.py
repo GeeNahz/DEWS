1003a3a613b1cb399456a370710a17a667de12ef
Othniel Eggah <emmaueleggah@gmail.com>
2023-12-20 19:28:26 +0100

    Feat: create temperature and precipitation models

--- apps/dewsDC/models.py
+++ apps/dewsDC/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from ..utils import DefaultModelFields
+
+class Temperature(DefaultModelFields, models.Model):
+    temperature = models.FloatField(db_index=True)
+
+    def __str__(self):
+        return f'Temperature: {self.temperature}'
+
+
+class Precipitation(DefaultModelFields, models.Model):
+    precipitation = models.FloatField(db_index=True)
+
+    def __str__(self):
+        return f'Precipitation: {self.precipitation}'
