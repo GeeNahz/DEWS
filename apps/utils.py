ee959f7e0e667f4742daf513999660df1223172f
Othniel Eggah <emmaueleggah@gmail.com>
2023-12-20 19:36:34 +0100

    Fix: remove function call parenthesis for uuid4

--- apps/utils.py
+++ apps/utils.py
@@ -5,7 +5,7 @@ from django_extensions.db.models import TimeStampedModel
 
 
 class DefaultModelFields(TimeStampedModel, models.Model):
-    id = models.UUIDField('id', primary_key=True, default=uuid.uuid4(), editable=False, db_index=True)
+    id = models.UUIDField('id', primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
 
     class Meta:
         abstract = True
