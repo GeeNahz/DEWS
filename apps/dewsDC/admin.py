fd12e37015d3c081f7468e417d9a9caeea99b00e
Othniel Eggah <emmaueleggah@gmail.com>
2023-12-20 19:29:10 +0100

    Feat: register models to admin panel

--- apps/dewsDC/admin.py
+++ apps/dewsDC/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Temperature, Precipitation
+
+
+admin.site.register(Temperature)
+admin.site.register(Precipitation)
