96d0c39d576e60955fafb7057e374bda70e9aaba
Godswill Nasara <godswillnlm@gmail.com>
2024-01-13 11:20:42 +0100

    Feat: create SensorData model to take sensor readings

--- apps/dewsDC/models.py
+++ apps/dewsDC/models.py
@@ -2,31 +2,45 @@ from django.db import models
 
 from ..utils import DefaultModelFields
 
-class Temperature(DefaultModelFields, models.Model):
+
+class SensorData(DefaultModelFields, models.Model):
     temperature = models.FloatField(db_index=True)
-
-    class Meta:
-        ordering = ['-created']
-    
-    def __str__(self):
-        return f'Temperature: {self.temperature}'
-
-
-class Precipitation(DefaultModelFields, models.Model):
+    humidity = models.FloatField(db_index=True)
     precipitation = models.FloatField(db_index=True)
-
-    class Meta:
-        ordering = ['-created']
-    
-    def __str__(self):
-        return f'Precipitation: {self.precipitation}'
-
-
-class SoilMoisture(DefaultModelFields, models.Model):
     soil_moisture = models.FloatField(db_index=True)
 
     class Meta:
         ordering = ['-created']
     
     def __str__(self):
-        return f'SoilMoisture: {self.soil_moisture}'
+        return f'Temp: {self.temperature}; Hum: {self.humidity}; Prec: {self.precipitation}; moist: {self.soil_moisture}'
+
+
+# class Temperature(DefaultModelFields, models.Model):
+#     temperature = models.FloatField(db_index=True)
+
+#     class Meta:
+#         ordering = ['-created']
+    
+#     def __str__(self):
+#         return f'Temperature: {self.temperature}'
+
+
+# class Precipitation(DefaultModelFields, models.Model):
+#     precipitation = models.FloatField(db_index=True)
+
+#     class Meta:
+#         ordering = ['-created']
+    
+#     def __str__(self):
+#         return f'Precipitation: {self.precipitation}'
+
+
+# class SoilMoisture(DefaultModelFields, models.Model):
+#     soil_moisture = models.FloatField(db_index=True)
+
+#     class Meta:
+#         ordering = ['-created']
+    
+#     def __str__(self):
+#         return f'SoilMoisture: {self.soil_moisture}'
