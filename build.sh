485e8a695b6c1f6f894c267731e3d83b5da4489a
Godswill Nasara <godswillnlm@gmail.com>
2024-01-13 11:11:51 +0100

    Update: add collectstatic command to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 set -o errexit # exit on error
 
+python -m pip install --upgrade pip
+
 pip install -r requirements.txt
 
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
