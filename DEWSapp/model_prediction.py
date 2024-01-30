72b0b835895ff6c00805944835d1ac95fe9be27b
Godswill Nasara <godswillnlm@gmail.com>
2024-01-30 12:15:10 +0100

    Feat: add thresholding to the spei value

--- DEWSapp/model_prediction.py
+++ DEWSapp/model_prediction.py
@@ -3,20 +3,26 @@ import pandas as pd
 
 from django.conf import settings
 
+from DEWSapp.threshold_constants import thresholds
+
 model_pathname = settings.MODEL_FILE_PATH
-historical_data_pathname = settings.BASE_DIR / "DEWSapp" / "dewsdocs" / "Shiroro-Spei.csv"
+historical_data_pathname = (
+    settings.BASE_DIR / "DEWSapp" / "dewsdocs" / "Shiroro-Spei.csv"
+)
 
 
 class ModelPrediction:
     @staticmethod
     def determine_forecast_steps(historical_data, current_year):
-        latest_year_historical_data = pd.DatetimeIndex(historical_data['Date']).year.max()
-        
+        latest_year_historical_data = pd.DatetimeIndex(
+            historical_data["Date"]
+        ).year.max()
+
         year_diff = int(current_year) - int(latest_year_historical_data)
 
         steps = year_diff * 12
         return steps
-    
+
     @staticmethod
     def forecast_spei(year):
         """
@@ -27,40 +33,70 @@ class ModelPrediction:
         """
 
         # Load the saved ARIMA model
-        with open(model_pathname, 'rb') as model_file:
+        with open(model_pathname, "rb") as model_file:
             model = pickle.load(model_file)
-        
+
         # Load saved historical data
         historical_spei = pd.read_csv(historical_data_pathname)
 
-        latest_date = historical_spei['Date'].max()
+        latest_date = historical_spei["Date"].max()
         forecast_start_date = pd.to_datetime(latest_date) + pd.DateOffset(months=1)
 
-        forecast_steps = ModelPrediction.determine_forecast_steps(historical_spei, year) # Adjust as needed
+        forecast_steps = ModelPrediction.determine_forecast_steps(
+            historical_spei, year
+        )  # Adjust as needed
 
-        forecast_index = pd.date_range(forecast_start_date, periods=forecast_steps, freq='M')
+        forecast_index = pd.date_range(
+            forecast_start_date, periods=forecast_steps, freq="M"
+        )
         forecast = model.get_forecast(steps=forecast_steps)
         forecast_values = forecast.predicted_mean
-        
-        forecast_data = pd.DataFrame({'Date': forecast_index, 'Forecasted_SPEI': forecast_values})
+
+        forecast_data = pd.DataFrame(
+            {"Date": forecast_index, "Forecasted_SPEI": forecast_values}
+        )
 
         return forecast_data
+
+    @staticmethod
+    def threshold(spei_value: int | float):
+        index = thresholds(value=spei_value)
+
+        data = {
+            "spei": round(spei_value, 3),
+            "drought_index": index,
+        }
+
+        return data
 
     @staticmethod
     def extract_spei(forecast_spei: pd.DataFrame, month: int, year: int):
         # forecast_spei = forecast_spei.drop(index=0)
 
-        forecast_year = forecast_spei[pd.DatetimeIndex(forecast_spei['Date']).year == year]
-        required_forecast = forecast_year[pd.DatetimeIndex(forecast_year['Date']).month == month]
+        forecast_year = forecast_spei[
+            pd.DatetimeIndex(forecast_spei["Date"]).year == year
+        ]
+        required_forecast = forecast_year[
+            pd.DatetimeIndex(forecast_year["Date"]).month == month
+        ]
 
-        return required_forecast.iat[0,1]
+        return required_forecast.iat[0, 1]
 
     @staticmethod
     def predict(month, year):
         spei_value = ModelPrediction.extract_spei(
             forecast_spei=ModelPrediction.forecast_spei(year),
             month=int(month),
-            year=int(year)
+            year=int(year),
         )
 
-        print(spei_value)
+        data = ModelPrediction.threshold(spei_value=spei_value)
+
+        result = {
+            "Month": month,
+            "Year": year,
+            **data,
+        }
+
+        print(result)
+        return result
