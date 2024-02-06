aa145a02e1df16057ebbf51f0fd9afaeb168c012
Godswill Nasara <godswillnlm@gmail.com>
2024-02-06 08:46:00 +0100

    Feat: add postgresql connection string

--- DEWSproject/settings/settings_prod.py
+++ DEWSproject/settings/settings_prod.py
@@ -1,4 +1,5 @@
 from .base_settings import *
+import dj_database_url
 
 SECRET_KEY = env('SECRET_KEY')
 
@@ -11,5 +12,12 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600,
+    )
+}
 
 django_heroku.settings(locals())
