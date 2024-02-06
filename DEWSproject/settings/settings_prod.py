8270a9dd18d916470542ce108a8c3965e57cdebf
Godswill Nasara <godswillnlm@gmail.com>
2024-02-06 08:39:54 +0100

    Update: update the allowed hosts list
    add localhost and render external hostname to list of allowed host

--- DEWSproject/settings/settings_prod.py
+++ DEWSproject/settings/settings_prod.py
@@ -4,8 +4,11 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env("DEBUG")
 
-# ALLOWED_HOSTS = ['http://dewsapp.herokuapp.com', 'https://dewsapp.herokuapp.com', '127.0.0.1']
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost']
+
+RENDER_EXTERNAL_HOSTNAME = env('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
