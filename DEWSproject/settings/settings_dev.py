9979a91e1ecb8fbd47f2899967fb5c4b77897e96
Godswill Nasara <godswillnlm@gmail.com>
2024-02-06 08:43:25 +0100

    Refactor: move database settings to dev settings

--- DEWSproject/settings/settings_dev.py
+++ DEWSproject/settings/settings_dev.py
@@ -5,3 +5,14 @@ SECRET_KEY = "django-insecure-ps3NjCrathmApVFOW1xRKKeb+OVaxyAzyLDztSYcC4w="
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
