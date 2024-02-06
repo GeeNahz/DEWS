5f1b36c6f4d1138ce2096441ffd61564665ab1e0
Godswill Nasara <godswillnlm@gmail.com>
2024-02-06 09:30:10 +0100

    Feat: add database url from env

--- DEWSproject/settings/settings_prod.py
+++ DEWSproject/settings/settings_prod.py
@@ -11,9 +11,11 @@ RENDER_EXTERNAL_HOSTNAME = env("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+DATABASE_URL = env("DATABASE_URL")
+
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgresql://postgres:postgres@localhost:5432/mysite",
+        default=DATABASE_URL,
         conn_max_age=600,
     )
 }
